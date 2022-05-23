0eab395a16962435ba1f4fef1ca60a35a6b5a4f0
tinhthanh <k40cntt@gmail.com>
2022-05-23 10:37:11 +0700

    Finishing the contact form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import ListContacts from "./components/ListContracts";
+import CreateContact from "./components/CreateContact";
+import * as ContactsAPI from "./utils/ContactsAPI";
+import { Route } from "react-router-dom";
 class App extends Component {
+  state = {
+    contacts: [],
+    screen: "list",
+  };
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState(() => ({
+        contacts,
+      }));
+    });
+  }
+  onDeleteContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id;
+      }),
+    }));
+  };
   render() {
     return (
       <div>
-        Hello World
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.onDeleteContact}
+            />
+          )}
+        />
+        <Route path="/create" component={CreateContact} />
       </div>
     );
   }
